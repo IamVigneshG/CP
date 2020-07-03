50a9eea405f5c3a8af1d15834d3f3b6e63aa4e7a
VIGNESH G <43438142+IamVigneshG@users.noreply.github.com>
2020-07-03 09:50:26 +0530

    Update TrainSolution.java

--- Java/TrainSolution.java
+++ Java/TrainSolution.java
@@ -4,7 +4,10 @@ import java.io.* ;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.*;
-
+/*
+Question URL 
+https://www.hackerearth.com/practice/basic-programming/input-output/basics-of-input-output/practice-problems/algorithm/seating-arrangement-1/description
+*/
 class TrainSolution {
 
     public static void main( String args[] ) throws IOException {
