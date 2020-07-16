cf8bfd19e48fb749cb5a2fc15663d966664ed344
vignesh.govindhasamy <vignesh.govindhasamy@zohocorp.com>
2020-07-16 09:02:11 +0530

    TimeComplexity 1

---
+++ CPP/Sum.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+#include <iostream>
+#include <boost/multiprecision/cpp_int.hpp>
+using namespace boost::multiprecision;
+using namespace std;
+
+// https://www.hackerearth.com/practice/basic-programming/complexity-analysis/time-and-space-complexity/practice-problems/algorithm/a-b-4/submissions/
+int main()
+{
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+	/* code */
+#endif
+cpp_int int a,b;
+while(cin >> a >> b){
+   cout<<a+b<<"\n";
+}
+
+
+   
+}
